179afd6e5bba24dc0c70be706081f30927083801
jackluson <18219112108@163.com>
2020-10-31 02:42:18 +0800

    feat: adjust swaggerUrl if empty

--- mock.config.ts
+++ mock.config.ts
@@ -1,7 +1,7 @@
 const config = {
   port: 9009, // 启动端口
   localPath: '', // 本地swagger 配置文件绝对路径(遍历该文件下下面所有文件)
-  selectedTag: '官网', // 对应swagger config 的tab，默认全部， 配置的话， 只启动该tag下面的接口
+  selectedTag: '', // 对应swagger config 的tab，默认全部， 配置的话， 只启动该tag下面的接口
   swaggerUrl: 'https://dev-oa.vipthink.cn/oa/v2/api-docs?group=COMPLEX', // swagger config 接口路劲
   isLocalOpenRedis: false, // 是否开始redis存储swagger 配置（一般用于开发阶段）
   codeMap: {
